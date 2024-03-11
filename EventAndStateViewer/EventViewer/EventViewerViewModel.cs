d83efb4487a28847fa1e8708785bf55bd2782414
KaarelNoole <kaarelnoole@gmail.com>
2024-03-11 12:49:51 +0200

    Add Events goes to db

--- EventAndStateViewer/EventViewer/EventViewerViewModel.cs
+++ EventAndStateViewer/EventViewer/EventViewerViewModel.cs
@@ -2,14 +2,12 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data.SqlClient;
 using System.Windows.Input;
 using VideoOS.Platform.EventsAndState;
 
 namespace EventAndStateViewer.EventViewer
 {
-    /// <summary>
-    /// View model for EventViewerControl.xaml
-    /// </summary>
     class EventViewerViewModel : ViewModelBase
     {
         public string TabName => "Event viewer";
@@ -26,18 +24,52 @@ namespace EventAndStateViewer.EventViewer
 
         private void OnEventsReceived(object sender, IEnumerable<Event> events)
         {
+            
+            string connectionString = "Data Source=10.100.80.67;Initial Catalog=minubaas;User ID=minunimi;Password=test;";
+
+            
+            string insertQuery = "INSERT INTO Camera (EventTime, Source, Event) VALUES (@EventTime, @Source, @Event)";
+
             foreach (var @event in events)
             {
                 Events.Add(new EventViewModel(@event));
-                Console.WriteLine(@event.Source);
-                Console.WriteLine(@event.Time);
-                Console.WriteLine(@event.Type);
-            }
+                DateTime eventTime = @event.Time;
+                string source = @event.Source;
+                string eventText = @event.Type.ToString(); 
+        
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
 
-            // Remove oldest events to limit the list to 100
-            while (Events.Count > 100)
-            {
-                Events.RemoveAt(0);
+                    
+                    using (SqlCommand command = new SqlCommand(insertQuery, connection))
+                    {
+                        
+                        command.Parameters.AddWithValue("@EventTime", eventTime);
+                        command.Parameters.AddWithValue("@Source", source);
+                        command.Parameters.AddWithValue("@Event", eventText);
+
+                        try
+                        {
+                            
+                            int rowsAffected = command.ExecuteNonQuery();
+
+                            
+                            if (rowsAffected > 0)
+                            {
+                                Console.WriteLine("Record inserted successfully.");
+                            }
+                            else
+                            {
+                                Console.WriteLine("No records inserted.");
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("Error: " + ex.Message);
+                        }
+                    }
+                }
             }
         }
 
